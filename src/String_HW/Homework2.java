88051f655364a199637a462f58aba28077786a78
Valeriya <sirtsova.v@gmai.com>
2021-02-25 00:39:07 -0500

    Homework2 Datatypes

---
+++ src/String_HW/Homework2.java
@@ -0,0 +1,29 @@
+package String_HW;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+        byte hoursInDay = 24;
+        System.out.println("Number of hours in a day "+ hoursInDay);
+
+        short daysInTheYear = 366;
+        System.out.println("Max no. of days in a year is "+ daysInTheYear);
+
+        int amazonEmployees = 798000;
+        System.out.println("Total number of employees in Amazon "+ amazonEmployees);
+
+        long populationNum = 7670000000L;
+        System.out.println("Worldwide population is "+populationNum+ " cin 2021");
+
+        float interest = 2.8f;
+        System.out.println("Interest rate "+interest);
+
+        double accBalance = 32850.76;
+        System.out.println("Balance in the account is "+accBalance);
+
+        char firstName = 'V';
+        char lastName = 'S';
+        System.out.println("Initials of my name "+firstName+lastName);
+
+    }
+}
