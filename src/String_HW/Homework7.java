4934b2466d974a33b81db203ab54c9c8d86b1f8b
Valeriya <sirtsova.v@gmai.com>
2021-03-17 21:18:50 -0400

    .

---
+++ src/String_HW/Homework7.java
@@ -0,0 +1,67 @@
+package String_HW;
+
+import java.util.Locale;
+
+public class Homework7 {
+    public static void main(String[] args) {
+        /**
+         * String sentence1 =  "hApPY nEW yeAr";
+         * sout(sentence1);  // hApPY nEW yeAr
+         * // code
+         * sout(sentence1);  // Happy New Year
+         */
+
+        String sentence1 = "hApPY nEW yeAr";
+        System.out.println(sentence1);
+        String sentenceLower = sentence1.toLowerCase();
+        String[] sentenceArr = sentenceLower.split(" ");
+        for (int i = 0; i < sentenceArr.length; i++) {
+            String firstLetter = sentenceArr[i].substring(0, 1).toUpperCase();
+            String remainingLetters = sentenceArr[i].substring(1);
+            if (i >= sentenceArr.length) {
+                break;
+            }
+            System.out.print(firstLetter + remainingLetters + " ");
+        }
+        System.out.println("\n");
+        /**
+         * Create abbreviation:
+         * String sentence2 =  "Lab sessIONS clAsses";
+         * // code
+         * LSC
+         *
+         */
+        String sentence2 = "Lab sessIONS clAsses";
+        String[] sentence_2_Arr = sentence2.split(" ");
+        for (int i = 0; i < sentence_2_Arr.length; i++) {
+            String abbreviationLetter = sentence_2_Arr[i].substring(0, 1).toUpperCase();
+            System.out.print(abbreviationLetter);
+        }
+
+        System.out.println("\n");
+
+        /**
+         * Reverse a string
+         *
+         */
+        // String sentence3 = "happy holidays";
+        // String reverseSentence3 = "";
+        //  System.out.println("sentence3 : " + sentence3);     // happy holidays
+        // code
+        // System.out.println("Reverse sentence3 : " + reverseSentence3);  // syadiloh yppah
+        String sentence3 = "happy holidays";
+
+        System.out.println("sentence3 : " + sentence3);
+        String reverseSentence3 = "";
+        System.out.print("Reverse sentence3 : ");
+      for (int i=sentence3.length()-1; i>=0; i--) {
+            System.out.print(sentence3.charAt(i));
+                }
+    }
+}
+
+
+
+
+
+
--- src/UD_class/Interview_alghoritms.java
+++ src/UD_class/Interview_alghoritms.java
@@ -5,11 +5,30 @@ public class Interview_alghoritms {
         //  How to reverse a String in java? Can you write a program without using any java inbuilt methods?
         String myString = "Valeriya Sirtsova63424";
 
-        for (int i = myString.length()-1; i>=0; i--) {
+        for (int i = myString.length() - 1; i >= 0; i--) {
             System.out.print(myString.charAt(i));
         }
 
+        String[] wordsArr = {"hello", "peace", "valerie", "smile", "cat", "long", "short", "mine", "yours"};
+        int x = wordsArr.length - 1;
+        for (int i = wordsArr.length - 1; i >= 0; i--) {
+            System.out.println(wordsArr[i] + " ");
+        }
 
+
+        for (int i = 1; i < 5; i++) {
+            System.out.println("hello");
+        }
+        System.out.println("\n");
+        for (int i = 0; i < wordsArr.length; i+=2) {
+
+            System.out.println(wordsArr[i]);
+        }
+
+        }
     }
-}
+
+
+
+
 
